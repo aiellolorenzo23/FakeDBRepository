3cfe40b31f625f29f2f2f4edeb731371c8ba35da
laiello <aiellolorenzo23@gmail.com>
2026-05-19 12:18:25 +0200

    first commit

---
+++ src/test/java/com/fakedb/fakedbrepository/FakeDbRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakedb.fakedbrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeDbRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
